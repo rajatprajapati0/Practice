393c85530abb183add69115fba7a0aad6348037a
Rajat <rajatprajapati09@gmail.com>
2023-03-12 09:32:40 +0530

    calculating Wage For 20 Working  day

--- Attendance.cs
+++ Attendance.cs
@@ -5,32 +5,39 @@ namespace EmployeeWage
 {
     public class Attendance
     {
+        public const int perHourWage = 20;
+        public const int fullDayHour = 2;
+        public const int halfDayHour = 1;
+        public const int workingDay = 20;
         public void  CheckAttendance()
         {
-            int perHourWage = 20;
-            int fullDayHour = 8;
-            int halfDayHour = 4;
+            int emphour = 0;
+            int dailywage = 0;
+            int monthlyWage = 0;
+       
             Random rnd = new Random();
-            int check= rnd.Next(0,3);
 
-
-            switch (check)
+           for (int day=0; day<workingDay;day++)
             {
-            
-             case 0:
-                    Console.WriteLine("employee is not present");
-                    break;
-             case 1:
-                    Console.WriteLine($"employee is present for half day\ndaily wage of employee is {perHourWage * halfDayHour}");
+                int check = rnd.Next(0, 3);
 
-                    break;
-             case 2:
-                    Console.WriteLine($"employee is present for half day\ndaily wage of employee is {perHourWage*fullDayHour}");
+                switch (check)
+                {
+                    case halfDayHour:
+                        emphour = 4;
+                        break;
+                    case fullDayHour:
+                        emphour = 8;
+                        break;
+                    default:
+                        emphour = 0;
+                        break;
 
-                    break;
-
-            }
-          
+                }
+                dailywage = emphour * perHourWage;
+                monthlyWage += dailywage;
+           }
+            Console.WriteLine($"monthly wage :{monthlyWage}");        
         }
     }
 }
