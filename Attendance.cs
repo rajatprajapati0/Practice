a2b254b56d4b43bb813e704654524e0d128a7b2f
Rajat <rajatprajapati09@gmail.com>
2023-03-12 09:52:05 +0530

    Calculating wage till hour=100 And day =20

--- Attendance.cs
+++ Attendance.cs
@@ -9,16 +9,20 @@ namespace EmployeeWage
         public const int fullDayHour = 2;
         public const int halfDayHour = 1;
         public const int workingDay = 20;
+        public const int maxHour = 100;
+     
         public void  CheckAttendance()
         {
             int emphour = 0;
-            int dailywage = 0;
+            int totalEmpHour = 0;
             int monthlyWage = 0;
+            int totalDay = 0;
+           
        
             Random rnd = new Random();
-
-           for (int day=0; day<workingDay;day++)
+            while (totalEmpHour <= maxHour&&totalDay<workingDay)
             {
+                totalDay++;
                 int check = rnd.Next(0, 3);
 
                 switch (check)
@@ -34,9 +38,11 @@ namespace EmployeeWage
                         break;
 
                 }
-                dailywage = emphour * perHourWage;
-                monthlyWage += dailywage;
-           }
+                totalEmpHour += emphour;
+                
+            }
+            monthlyWage =  totalEmpHour*perHourWage;
+
             Console.WriteLine($"monthly wage :{monthlyWage}");        
         }
     }
