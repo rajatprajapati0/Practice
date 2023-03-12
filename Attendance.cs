3a88472090289bc12fd0e030cfb75512712e2eb2
Rajat <rajatprajapati09@gmail.com>
2023-03-12 08:04:33 +0530

    half and full day attendance and calculating daily wage

--- Attendance.cs
+++ Attendance.cs
@@ -8,17 +8,22 @@ namespace EmployeeWage
         public void  CheckAttendance()
         {
             int perHourWage = 20;
-            int perDayHour = 8;
+            int fullDayHour = 8;
+            int halfDayHour = 4;
             Random rnd = new Random();
-            int check= rnd.Next(0,2);
+            int check= rnd.Next(0,3);
     
             if (check == 0) 
             {
                 Console.WriteLine("employee is not present");
             }
+            if (check == 1)
+            {
+                Console.WriteLine($"employee is present for half day\ndaily wage of employee is {perHourWage*halfDayHour}");
+            }
             else
             {
-                Console.WriteLine($"employee is present \ndaily wage of employee is {perHourWage*perDayHour}");
+                Console.WriteLine($"employee is present for half day\ndaily wage of employee is {perHourWage*fullDayHour}");
             }
         }
     }
